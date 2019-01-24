bb18bf4bc17206d1282454b90956f4c0805eaf12
mark-marshall <mark.marshallgp@gmail.com>
2019-01-24 13:02:19 +0000

    [MVP] Lambda-classes: added Student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -84,25 +84,11 @@ class Person {
     this.gender = personAttrs.gender;
   }
   speak() {
-    return `Hello my name is ${this.name}, I am from ${this.location}`;
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
   }
 }
 
 // Instructor Constructor ============
-
-/* #### Instructor
-
-* Now that we have a Person as our base class, we'll build our Instructor class.
-* Instructor uses the same attributes that have been set up by Person
-* Instructor has the following unique props:
-  * `specialty` what the Instructor is good at i.e. 'redux'
-  * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-  * `catchPhrase` i.e. `Don't forget the homies`
-* Instructor has the following methods:
-  * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-  * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
-  * 
-  */
 
 class Instructor extends Person {
   constructor(instructorAttrs) {
@@ -115,6 +101,42 @@ class Instructor extends Person {
     return `Today we are learning about ${subject}`;
   }
   grade(student, subject) {
-    return `${student.name} receives a perfect score on {subject}`;
+    console.log(`${student.name} receives a perfect score on {subject}`);
   }
 }
+
+// Student Constructor ============
+
+/* #### Student
+
+* Now we need some students!
+* Student uses the same attributes that have been set up by Person
+* Student has the following unique props:
+  * `previousBackground` i.e. what the Student used to do before Lambda School
+  * `className` i.e. CS132
+  * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+* Student has the following methods:
+  * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+  * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+  * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+  */
+
+  class Student extends Person {
+      constructor(studentAttrs) {
+          super(studentAttrs);
+          this.previousBackground = studentAttrs.previousBackground;
+          this.className = studentAttrs.className;
+          this.favSubjects = studentAttrs.favSubjects;
+      }
+      listSubjects(){
+          for (let i=0; i<favSubjects.length; i++){
+              console.log(favSubjects[i]);
+          }
+      }
+      PRAssignment(subject) {
+          console.log(`${this.name} has submitted a PR for ${subject}`)
+      }
+      sprintChallenge(subject) {
+          console.log(`${this.name} has begun sprint challenge on ${subject}`)
+      }
+  }
