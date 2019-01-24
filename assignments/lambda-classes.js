7da599f3675e8bea859aa0f33bcbc42f71791332
mark-marshall <mark.marshallgp@gmail.com>
2019-01-24 12:54:53 +0000

    [MVP] Lambda-classes: added Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -74,23 +74,47 @@ const fred = new Instructor({
 
 // CODE here for your Lambda Classes
 
-/* #### Person
-
-* First we need a Person class. This will be our `base-class`
-* Person receives `name` `age` `location` `gender` all as props
-* Person receives `speak` as a method.
-* This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props */
-
 // Person Constructor ============
 
 class Person {
-    constructor(personAttrs) {
-        this.name = personAttrs.name;
-        this.age = personAttrs.age;
-        this.location = personAttrs.location;
-        this.gender = personAttrs.gender;
-    }
-    speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`
-    }
+  constructor(personAttrs) {
+    this.name = personAttrs.name;
+    this.age = personAttrs.age;
+    this.location = personAttrs.location;
+    this.gender = personAttrs.gender;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+// Instructor Constructor ============
+
+/* #### Instructor
+
+* Now that we have a Person as our base class, we'll build our Instructor class.
+* Instructor uses the same attributes that have been set up by Person
+* Instructor has the following unique props:
+  * `specialty` what the Instructor is good at i.e. 'redux'
+  * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
+  * `catchPhrase` i.e. `Don't forget the homies`
+* Instructor has the following methods:
+  * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+  * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+  * 
+  */
+
+class Instructor extends Person {
+  constructor(instructorAttrs) {
+    super(instructorAttrs);
+    this.speciality = instructorAttrs.speciality;
+    this.favLanguage = instructorAttrs.favLanguage;
+    this.catchPhrase = instructorAttrs.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on {subject}`;
+  }
 }
