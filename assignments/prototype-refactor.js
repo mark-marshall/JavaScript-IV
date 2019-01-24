c743353e476985f5d3568acdb752998cb907e485
mark-marshall <mark.marshallgp@gmail.com>
2019-01-24 12:44:15 +0000

    [MVP] All Javascript III code refactored and tested

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -44,7 +44,47 @@ Humanoid.prototype.greet = function() {
   return `${this.name} offers a greeting in ${this.language}`;
 };
 
-// Test you work by un-commenting these 3 objects and the list of console logs below:
+2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+*/
+
+// Game Object Constructor ============
+
+class GameObject {
+  constructor(gameObjectArgs) {
+    this.createdAt = gameObjectArgs.createdAt;
+    this.dimensions = gameObjectArgs.dimensions;
+  }
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+// Character Stats Constructor ============
+
+class CharacterStats extends GameObject {
+  constructor(characterStatsArgs) {
+    super(characterStatsArgs);
+    this.healthPoints = characterStatsArgs.healthPoints;
+    this.name = characterStatsArgs.name;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
+
+// Humanoid Constructor ============
+
+class Humanoid extends CharacterStats {
+    constructor (humanoidArgs) {
+        super(humanoidArgs);
+        this.team = humanoidArgs.team;
+        this.weapons = humanoidArgs.weapons;
+        this.language = humanoidArgs.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}`;
+    }
+}
 
 const mage = new Humanoid({
     createdAt: new Date(),
@@ -87,128 +127,16 @@ const mage = new Humanoid({
     weapons: ["Bow", "Dagger"],
     language: "Elvish"
   });
-  
-  const darth = new Villain({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 1,
-      height: 10
-    },
-    healthPoints: 200,
-    name: "Darth",
-    team: "None",
-    weapons: ["Lightsaber", "Stormtroopers"],
-    language: "English",
-    side: "Dark"
-  });
-  
-  const yoda = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 1,
-      height: -2
-    },
-    healthPoints: 1000,
-    name: "Yoda",
-    team: "Jedi",
-    weapons: ["Wisdom"],
-    language: "Yodish",
-    side: "Not Dark"
-  });
-  
-  // Testing MVP constructor functions ============
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
-
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
-
-*/
-
-/* Game Object Constructor ============
-
-function GameObject(gameObjectArgs) {
-    this.createdAt = gameObjectArgs.createdAt;
-    this.dimensions = gameObjectArgs.dimensions;
-  }
-  
-  GameObject.prototype.destroy = function() {
-    return `${this.name} was removed from the game.`;
-  };
-
-  */
-
-class GameObject {
-  constructor(gameObjectArgs) {
-    this.createdAt = gameObjectArgs.createdAt;
-    this.dimensions = gameObjectArgs.dimensions;
-  }
-  destroy() {
-    return `${this.name} was removed from the game.`;
-  }
-}
-
-/* Character Stats Constructor ============
-
-function CharacterStats(characterStatsArgs) {
-    GameObject.call(this, characterStatsArgs);
-    this.healthPoints = characterStatsArgs.healthPoints;
-    this.name = characterStatsArgs.name;
-  }
-  
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  
-  CharacterStats.prototype.takeDamage = function() {
-    return `${this.name} took damage.`;
-  };
-
-  */
-
-class CharacterStats extends GameObject {
-  constructor(characterStatsArgs) {
-    super(characterStatsArgs);
-    this.healthPoints = characterStatsArgs.healthPoints;
-    this.name = characterStatsArgs.name;
-  }
-  takeDamage() {
-    return `{this.name} took damage.`;
-  }
-}
-
-/* Humanoid Constructor ============
-
-function Humanoid(humanoidArgs) {
-    CharacterStats.call(this, humanoidArgs);
-    this.team = humanoidArgs.team;
-    this.weapons = humanoidArgs.weapons;
-    this.language = humanoidArgs.language;
-  }
-  
-  Humanoid.prototype = Object.create(CharacterStats.prototype);
-  
-  Humanoid.prototype.greet = function() {
-    return `${this.name} offers a greeting in ${this.language}`;
-  };
-*/
-
-class Humanoid extends CharacterStats {
-    constructor (humanoidArgs) {
-        super(humanoidArgs);
-        this.team = humanoidArgs.team;
-        this.weapons = humanoidArgs.weapons;
-        this.language = humanoidArgs.language;
-    }
-    greet() {
-        return `${this.name} offers a greeting in ${this.language}`;
-    }
-}
+   // Testing MVP constructor functions ============
+   console.log(mage.createdAt); // Today's date
+   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+   console.log(swordsman.healthPoints); // 15
+   console.log(mage.name); // Bruce
+   console.log(swordsman.team); // The Round Table
+   console.log(mage.weapons); // Staff of Shamalama
+   console.log(archer.language); // Elvish
+   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+   console.log(mage.takeDamage()); // Bruce took damage.
+   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+ 
