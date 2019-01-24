368640ee1b18b778374c931bad343e46778a431b
mark-marshall <mark.marshallgp@gmail.com>
2019-01-24 12:31:03 +0000

    [MVP] Refactored GameObject constructor

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -3,6 +3,7 @@
 Prototype Refactor
 
 1. Copy and paste your code or the solution from yesterday
+
 // Game Object Constructor ============
 
 function GameObject(gameObjectArgs) {
@@ -133,3 +134,27 @@ const mage = new Humanoid({
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+
+/* Game Object Constructor ============
+
+function GameObject(gameObjectArgs) {
+    this.createdAt = gameObjectArgs.createdAt;
+    this.dimensions = gameObjectArgs.dimensions;
+  }
+  
+  GameObject.prototype.destroy = function() {
+    return `${this.name} was removed from the game.`;
+  };
+
+  */
+
+  class GameObject {
+      constructor(gameObjectArgs) {
+        this.createdAt = gameObjectArgs.createdAt;
+        this.dimensions = gameObjectArgs.dimensions;
+      }
+      destroy() {
+          return `${this.name} was removed from the game.`;
+      }
+  }
